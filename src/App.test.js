81c27ab15fd4929668e74e5f7f9384daf5c24eb1
Vasusund <vasusund1977@gmail.com>
2025-09-29 13:10:25 +1000

    Commits for files

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Login from './components/Login';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders login heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/login/i);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const heading = screen.getByRole('heading', { name: /login/i });
   expect(heading).toBeInTheDocument();
 });
 
 test('login button exists', () => {
-  render(<Login />);
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
   const loginButton = screen.getByRole('button', { name: /login/i });
   expect(loginButton).toBeInTheDocument();
 });
